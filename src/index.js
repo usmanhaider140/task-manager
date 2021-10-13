7fa13151f455fa2f245827f19ac8f8062147d0ed
UsmanHaider <usmanhaider.falconit@gmail.com>
2021-10-13 14:58:20 +0500

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 const express = require("express");
 require("./db/mongoose");
-const chalk = require("chalk");
 const userRouter = require("./routes/user");
 const taskRouter = require("./routes/task");
-const bodyParser = require("body-parser");
+const { urlencoded, json } = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const port = process.env.PORT;
+app.use(urlencoded({ extended: false }));
+app.use(json());
 app.use(express.json());
-
-// Example Middleware
-// app.use((req, res, next) => {
-//   console.log(chalk.greenBright.inverse("MiddleWare"), req.method, req.path);
-//   if (req.method === "GET") {
-//     res.send("Get requests are disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send("maintenance error");
-// });
 
 app.use(userRouter);
 app.use(taskRouter);
@@ -34,18 +19,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
 });
-
-// Comment:: Without middleware new request --> run route handler
-
-// Comment:: With middleware new request --> do something --> run router handler
-
-// jwt.sign({_id: 123}, "key", {expiresIn: '20 seconds'})
-
-// const pet = {
-//   name: "billi",
-// };
-
-// pet.toJSON = function () {
-//   return {};
-// };
-// console.log(JSON.stringify(pet));
